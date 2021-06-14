0dde98f8b3512cc9ae51ecba0dfea2c77c601791
elskater98 <elskater98@gmail.com>
2021-06-14 21:56:50 +0200

    Get available countries

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,3 +1,6 @@
+import datetime
+import math
+
 import pandas as pd
 import requests
 import json
@@ -12,17 +15,43 @@ def get_countries(limit=200, order_by='country', sort='asc'):
     return response.json()
 
 
-def get_parameters():
-    pass
+def get_parameters(limit=100, sort='asc'):
+    query_params = {'limit': limit, 'sort': sort}
+    response = requests.get(url=API_BASE_URL + 'parameters', params=query_params, timeout=20)
+    return response.json()
 
 
-def save_json(input_name, data):
-    with open(RAW_DATA_PATH + input_name, 'w') as file:
+# TODO: add list of parameters
+def get_average_measurements(limit=100000, page=1, sort='desc', country_id='ES',
+                             spatial='country',
+                             temporal='day', date_from=datetime.date(2020, 1, 1), date_to=datetime.date(2020, 12, 31)):
+    query_params = {'limit': 1, 'sort': sort, 'page': page, 'country_id': country_id,
+                    'date_from': date_from,
+                    'date_to': date_to, 'spatial': spatial, 'temporal': temporal}
+
+    found = int(requests.get(url=API_BASE_URL + 'averages', params=query_params, timeout=20).json()['meta']['found'])
+    pages = math.ceil(found / limit)
+
+    for i in range(pages):
+        query_params = {'limit': limit, 'sort': sort, 'page': i + 1, 'country_id': country_id,
+                        'date_from': date_from,
+                        'date_to': date_to, 'spatial': spatial, 'temporal': temporal}
+        response = requests.get(url=API_BASE_URL + 'averages', params=query_params, timeout=30)
+
+        save_json('measurements_%s_%s.json' % (temporal, i), response.json())
+
+
+def save_json(input_name, data, type='w'):
+    with open(RAW_DATA_PATH + input_name, type) as file:
         json.dump(data, file, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
-    # Get countries
+    # Get and save countries
     countries = get_countries()
     save_json('countries.json', countries)
-    # Save countries
+
+    # Get and save parameters
+    parameters = get_parameters()
+    save_json('parameters.json', parameters)
+    get_average_measurements()
